41bffb1d62a8bbc0a00fa6f076ee77e02f61308b
Ruslan Bahirau <ruslan.bahirau.1999@gmail.com>
2019-04-03 17:34:56 +0300

    Add Solid Works client

---
+++ Lab5DrawLab1/SolidWorksClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System.Runtime.InteropServices;
+
+namespace Lab5DrawLab1
+{
+    public class SolidWorksClient
+    {
+        public SldWorks SwApp { get; private set; }
+        public ModelDoc2 SwModel { get; private set; }
+        public SketchManager SwSketchManager { get; private set; }
+        public SelectionMgr SwSelMgr { get; private set; }
+    }
+}
